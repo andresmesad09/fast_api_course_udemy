420eb2e2a2de369a35d1ad5dfcd4b2cda51fb5ce
Andres Mesa <andres.david@coinsquare.com>
2024-12-02 17:07:30 -0500

    post and black

--- project_1/books.py
+++ project_1/books.py
@@ -1,38 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 BOOKS = [
-    {
-        "title": "Title One",
-        "author": "Author One",
-        "category": "science"
-    },
-    {
-        "title": "Title Two",
-        "author": "Author Two",
-        "category": "science"
-    },
-    {
-        "title": "Title Three",
-        "author": "Author Three",
-        "category": "history"
-    },
-    {
-        "title": "Title Four",
-        "author": "Author Four",
-        "category": "math"
-    },
-    {
-        "title": "Title Five",
-        "author": "Author Five",
-        "category": "math"
-    },
-    {
-        "title": "Title Six",
-        "author": "Author Two",
-        "category": "math"
-    },
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Four", "category": "math"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"},
+    {"title": "Title Six", "author": "Author Two", "category": "math"},
 ]
 
 
@@ -61,7 +37,14 @@ async def read_category_by_query(category: str):
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get("author").casefold() == book_author.casefold() and \
-                book.get("category").casefold() == category.casefold():
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
             books_to_return.append(book)
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
